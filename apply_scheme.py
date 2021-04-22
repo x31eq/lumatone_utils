16e692150424134bf0acadb5c622f1c0fd5f2503
Graham Breed <gbreed@gmail.com>
2021-04-22 22:05:53 +0100

    Work with tunings that don't repeat every board

--- apply_scheme.py
+++ apply_scheme.py
@@ -13,6 +13,8 @@ parser = argparse.ArgumentParser(
         description='Apply a color scheme to a Lumatone .ltn mapping')
 parser.add_argument('-t', '--tonic', type=int, nargs='?', default=0,
                     help='MIDI reference pitch')
+parser.add_argument('-g', '--gap', type=int, nargs='?', default=0,
+                    help='Notes to offset from one channel to another')
 parser.add_argument('-i', '--input', nargs='?',
                     help='file to read the color scheme from')
 parser.add_argument('-o', '--output', nargs='?',
@@ -37,6 +39,7 @@ else:
         (line.strip() for line in sys.stdin)))
 
 pitches = [[0] * KEYS_PER_BOARD for _ in range(N_BOARDS)]
+channels = [[0] * KEYS_PER_BOARD for _ in range(N_BOARDS)]
 
 board = 0
 for line in lines:
@@ -45,6 +48,11 @@ for line in lines:
     elif line.startswith('Key_') and line.count('=') == 1:
         key, pitch = line[4:].strip().split('=')
         pitches[board][int(key)] = int(pitch)
+    elif line.startswith('Chan_') and line.count('=') == 1:
+        key, channel = line[5:].strip().split('=')
+        channel = int(channel)
+        if channel:
+            channels[board][int(key)] = channel
 
 board = 0
 output = open(args.output, 'w') if args.output else sys.stdout
@@ -54,7 +62,8 @@ for line in lines:
     if line.startswith('Col_') and line.count('=') == 1:
         key, _color = line[4:].strip().split('=')
         key = int(key)
-        pitch = pitches[board][key]
+        channel = channels[board][key]
+        pitch = pitches[board][key] + (channel * args.gap)
         color = scheme[(pitch - args.tonic) % period]
         line = "Col_{}={}".format(key, color)
     output.write(line + '\n')
--- extract_scheme.py
+++ extract_scheme.py
@@ -6,7 +6,8 @@ Extract a color scheme from a Lumatone mapping file
 
 import argparse, sys
 
-KEYS_PER_SECTION = 56
+N_BOARDS = 5
+KEYS_PER_BOARD = 56
 
 parser = argparse.ArgumentParser(
         description='Extract the color scheme from a Lumatone .ltn mapping')
@@ -14,32 +15,55 @@ parser.add_argument('-p', '--period', type=int, nargs='?', default=128,
                     help='number of steps to a period/octave')
 parser.add_argument('-t', '--tonic', type=int, nargs='?', default=0,
                     help='MIDI reference pitch')
+parser.add_argument('-g', '--gap', type=int, nargs='?', default=0,
+                    help='Notes to offset from one channel to another')
 parser.add_argument('-o', '--output', nargs='?',
                     help='file to write the scheme to')
 parser.add_argument('input_filename')
 args = parser.parse_args()
 
-pitches = [0] * KEYS_PER_SECTION
-colors = ['000000'] * KEYS_PER_SECTION
+pitches = [[0] * KEYS_PER_BOARD for _ in range(N_BOARDS)]
+channels = [[0] * KEYS_PER_BOARD for _ in range(N_BOARDS)]
+colors = [[None] * KEYS_PER_BOARD for _ in range(N_BOARDS)]
 
+board = 0
 with open(args.input_filename) as ltn:
     for line in ltn:
-        if line.startswith('Key_') and line.count('=') == 1:
+        if line.startswith('[Board'):
+            board = int(line[6])
+        elif line.startswith('Key_') and line.count('=') == 1:
             key, pitch = line[4:].strip().split('=')
-            pitches[int(key)] = (int(pitch) - args.tonic) % args.period
+            pitches[board][int(key)] = int(pitch)
         elif line.startswith('Col_') and line.count('=') == 1:
             key, color = line[4:].strip().split('=')
             if color != '000000':
-                colors[int(key)] = color
+                colors[board][int(key)] = color
+        elif line.startswith('Chan_') and line.count('=') == 1:
+            key, channel = line[5:].strip().split('=')
+            channel = int(channel)
+            if channel:
+                channels[board][int(key)] = channel
 
-scheme = ['000000'] * args.period
-for pitch, color in zip(pitches, colors):
-    scheme[pitch] = color
+scheme = [None] * args.period
+for board, keyspecs in enumerate(zip(pitches, channels, colors)):
+    for pitch, channel, color in zip(*keyspecs):
+        if channel and color:
+            relative_pitch = (pitch + channel * args.gap) - args.tonic
+            relative_pitch %= args.period
+            old_color = scheme[relative_pitch]
+            if old_color and old_color != color:
+                sys.stderr.write(
+                        "Inconsistent color board {}, channel {}, pitch {}, "
+                        "relative pitch {}, color {}, old color {}\n"
+                                .format(board, channel, pitch, relative_pitch,
+                                    color, old_color))
+            else:
+                scheme[relative_pitch] = color
 
 if args.output:
     with open(args.output, 'w') as result:
         for pitch, color in enumerate(scheme):
-            result.write("{}\n".format(color))
+            result.write("{}\n".format(color or '000000'))
 else:
     for pitch, color in enumerate(scheme):
-        sys.stdout.write("{}\n".format(color))
+        sys.stdout.write("{}\n".format(color or '000000'))
