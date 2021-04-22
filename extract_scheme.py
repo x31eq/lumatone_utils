0afd919ec44056c1362887b87b4e943e10013039
Graham Breed <gbreed@gmail.com>
2021-04-22 19:49:34 +0100

    Accept an output filename

--- extract_scheme.py
+++ extract_scheme.py
@@ -12,6 +12,8 @@ parser = argparse.ArgumentParser(
         description='Extract color scheme from a Lumatone .ltn mapping')
 parser.add_argument('-p', '--period', type=int, nargs='?', default=128,
                     help='number of steps to a period/octave')
+parser.add_argument('-o', '--output', nargs='?',
+                    help='file to write the scheme to')
 parser.add_argument('input_filename')
 args = parser.parse_args()
 
@@ -33,5 +35,10 @@ scheme = ['000000'] * args.period
 for pitch, color in zip(pitches, colors):
     scheme[pitch] = color
 
-for pitch, color in enumerate(scheme):
-    sys.stdout.write("{}={}\n".format(pitch, color))
+if args.output:
+    with open(args.output, 'w') as result:
+        for pitch, color in enumerate(scheme):
+            result.write("{}={}\n".format(pitch, color))
+else:
+    for pitch, color in enumerate(scheme):
+        sys.stdout.write("{}={}\n".format(pitch, color))
