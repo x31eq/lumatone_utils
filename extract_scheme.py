9ff90cc0ee87ef24e4b038e6229f67a1d4108890
Graham Breed <gbreed@gmail.com>
2021-04-22 19:42:26 +0100

    Reduce the colors modulo a period

--- extract_scheme.py
+++ extract_scheme.py
@@ -4,23 +4,34 @@
 Extract a color scheme from a Lumatone mapping file
 """
 
-import sys
+import argparse, sys
 
 KEYS_PER_SECTION = 56
 
-input_filename = sys.argv[1]
+parser = argparse.ArgumentParser(
+        description='Extract color scheme from a Lumatone .ltn mapping')
+parser.add_argument('-p', '--period', type=int, nargs='?', default=128,
+                    help='number of steps to a period/octave')
+parser.add_argument('input_filename')
+args = parser.parse_args()
+
+input_filename = args.input_filename
 
 pitches = [0] * 56
 colors = ['000000'] * 56
 
-with open(input_filename) as ltn:
+with open(args.input_filename) as ltn:
     for line in ltn:
         if line.startswith('Key_') and line.count('=') == 1:
             key, pitch = line[4:].strip().split('=')
-            pitches[int(key)] = int(pitch)
+            pitches[int(key)] = int(pitch) % args.period
         elif line.startswith('Col_') and line.count('=') == 1:
             key, color = line[4:].strip().split('=')
             colors[int(key)] = color
 
+scheme = ['000000'] * args.period
 for pitch, color in zip(pitches, colors):
+    scheme[pitch] = color
+
+for pitch, color in enumerate(scheme):
     sys.stdout.write("{}={}\n".format(pitch, color))
